9d6ea54e9553116107c8de2cbaec72c649f6745a
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-06-24 16:23:50 +0200

    Update run_baseline.sh

--- scripts/run_baseline.sh
+++ scripts/run_baseline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
-./automl/run_baseline.py
+python automl/run_baseline.py
 deactivate
