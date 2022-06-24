829716d505ca598b6110ba24171b52ac0d972be5
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-06-24 10:13:18 +0200

    Fix start script

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 docker stop hamlet_just_automl
 docker rm hamlet_just_automl
 docker build -t hamlet_just_automl .
-docker run --name hamlet_just_automl --volume %cd%/automl:/home/automl --volume %cd%/resources:/home/resources --detach -t hamlet_just_automl
+docker run --name hamlet_just_automl --volume %cd%:/home --detach -t hamlet_just_automl
 docker exec hamlet_just_automl bash ./run_experiments.sh
