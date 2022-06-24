6f9ba650d1b44eadd5789a31425d33ae89331786
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-06-24 16:56:42 +0200

    Update run_baseline.sh

--- scripts/run_baseline.sh
+++ scripts/run_baseline.sh
@@ -3,6 +3,7 @@ wget https://github.com/QueueInc/HAMLET/releases/download/$5/hamlet-$5-all.jar
 python3 -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+python -m pip install openml
+python -m pip install tqdm
 python automl/run_baseline.py --workspace $1 --metric $2 --mode $3 --batch_size $4 --version $5
 deactivate
