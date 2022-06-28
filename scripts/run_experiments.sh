b61cfea7817b9f908960eaa0e35075e75fd523a6
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-06-28 09:34:06 +0200

    Test time budget

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python automl/main.py --dataset 29 --metric "balanced_accuracy" --mode "max" --batch_size 250 --input_path "resources_nn/automl/input/automl_input_3.json" --output_path "resources_nn/automl/output/automl_output_3.json" --seed 42
+python automl/main.py --dataset 29 --metric "balanced_accuracy" --mode "max" --batch_size 5000 --input_path "temp/automl_input.json" --output_path "temp/automl_output.json" --seed 42
