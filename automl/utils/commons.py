99c42a4c40c52f5ed32d7443dc7fc1cfffc18b60
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-07-01 08:38:17 +0200

    Update commons.py

--- automl/utils/commons.py
+++ automl/utils/commons.py
@@ -1,5 +1,16 @@
-from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 
-algorithms = ["GaussianNB", "KNeighborsClassifier", "DecisionTreeClassifier"]
+algorithms = [
+    "GaussianNB",
+    "KNeighborsClassifier",
+    "DecisionTreeClassifier",
+    "MLPClassifier",
+    "SVC",
+    "RandomForestClassifier",
+    "AdaBoostClassifier",
+]
