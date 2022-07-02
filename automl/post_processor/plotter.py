0ff7063dd2e6e1d58798208b04eadff4964014d2
Giuseppe Pisano <giuseppe.pisano3@studio.unibo.it>
2022-07-02 14:13:03 +0000

    plotter

--- automl/post_processor/plotter.py
+++ automl/post_processor/plotter.py
@@ -3,6 +3,15 @@ import json
 import os
 import pandas as pd
 
+
+def get_best_in(target, evaluated_rewards):
+    filtered = [
+        x["balanced_accuracy"] if x["balanced_accuracy"] != "-inf" else 0
+        for x in evaluated_rewards[:target]
+    ]
+    return max(filtered)
+
+
 data = {}
 path = os.path.join("/", "home", "results")
 for approach in ["baseline_5000", "hamlet_250", "hamlet_150"]:
@@ -10,7 +19,8 @@ for approach in ["baseline_5000", "hamlet_250", "hamlet_150"]:
         for dataset, result in json.load(f).items():
             if dataset not in data:
                 data[dataset] = {}
-            data[dataset][approach] = result["best_config"]["balanced_accuracy"]
+            # data[dataset][approach] = result["best_config"]["balanced_accuracy"]
+            data[dataset][approach] = get_best_in(1000, result["evaluated_rewards"])
 
 
 pd.DataFrame.from_dict(data, orient="index").to_csv(os.path.join(path, "summary.csv"))
