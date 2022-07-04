c1290b8829f59d63f1c8bd8f206e5325d0bec114
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-07-04 18:36:34 +0200

    Update run_hamlet.sh

--- scripts/run_hamlet.sh
+++ scripts/run_hamlet.sh
@@ -6,5 +6,5 @@ python -m pip install --upgrade pip
 python -m pip install openml
 python -m pip install tqdm
 python -m pip install pandas
-python automl/run_baseline.py --workspace $1 --metric $2 --mode $3 --batch_size $4 --time_budget $5 --version $6 --range $7 --num_tasks $8 --iterations $9
+python automl/run_hamlet.py --workspace $1 --metric $2 --mode $3 --batch_size $4 --time_budget $5 --version $6 --range $7 --num_tasks $8 --iterations $9
 deactivate
