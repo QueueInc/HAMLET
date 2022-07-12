89a17ba61e95ff281c9fb22e49e380fe8a7d633b
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-07-12 10:22:50 +0200

    Update plotter.py

--- automl/post_processor/plotter.py
+++ automl/post_processor/plotter.py
@@ -34,53 +34,87 @@ def plot_pd(df, mode, other, path):
 
 def plot_matplotlib(df, mode, other, path):
 
-    labels = list(df.index)
+    df[
+        [f"argumentation_time_{x}" for x in other] + [f"automl_time_{x}" for x in other]
+    ].plot.bar().get_figure().savefig(os.path.join(path, f"time_{mode}.png"))
+
+    labels = df["name"]
     x = np.arange(len(labels))  # the label locations
-    width = 0.25  # the width of the bars
+    width = 0.2  # the width of the bars
 
     fig, ax = plt.subplots()
-    ax.bar(x - width, df["delta_hamlet_1000_kb3"], width, label="PKB")
-    ax.bar(x, df["delta_hamlet_250"], width, label="IKA")
-    ax.bar(x + width, df["delta_hamlet_250_kb3_fixed"], width, label="PKB + IKA")
-    ax.set_ylabel("Improvement")
-    ax.set_title("Improvement of balanced accuracy w.r.t. the baseline")
+    ax.bar(x - (width / 2 * 3), df["baseline_5000"], width, label="baseline")
+    ax.bar(x - width / 2, df["hamlet_1000_kb3"], width, label="PKB")
+    ax.bar(x + width / 2, df["hamlet_250"], width, label="IKA")
+    ax.bar(x + (width / 2 * 3), df["hamlet_250_kb3_fixed"], width, label="PKB + IKA")
+    ax.set_ylabel("Balanced accuracy")
+    ax.set_title("Balanced accuracy achieved by the approaches")
     ax.set_xticks(x, labels)
-    ax.legend()
-    fig.tight_layout()
-    fig.savefig(os.path.join(path, f"barchart_delta.png"))
+    ax.set_ylim([0.75, 1])
+    # ax.legend()
 
-    labels = list(df.index)
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    lgd = fig.legend(
+        by_label.values(),
+        by_label.keys(),
+        loc="lower center",
+        ncol=4,
+        bbox_to_anchor=(0.5, -0.05),
+    )
+    text = fig.text(-0.2, 1.05, "", transform=ax.transAxes)
+    fig.tight_layout()
+    fig.savefig(
+        os.path.join(path, f"barchart_delta.png"),
+        bbox_extra_artists=(lgd, text),
+        bbox_inches="tight",
+    )
+
+    labels = df["name"]
     x = np.arange(len(labels))  # the label locations
-    width = 0.25  # the width of the bars
+    width = 0.2  # the width of the bars
     fig, ax = plt.subplots()
-    ax.bar(x - width, abs(df["delta_iteration_hamlet_1000_kb3"]), width, label="PKB")
-    ax.bar(x, abs(df["delta_iteration_hamlet_250"]), width, label="IKA")
+    ax.bar(x - (width / 2 * 3), df["iteration_baseline_5000"], width, label="baseline")
+    ax.bar(x - width / 2, df["iteration_hamlet_1000_kb3"], width, label="PKB")
+    ax.bar(x + width / 2, df["iteration_hamlet_250"], width, label="IKA")
     ax.bar(
-        x + width,
-        abs(df["delta_iteration_hamlet_250_kb3_fixed"]),
+        x + (width / 2 * 3),
+        df["iteration_hamlet_250_kb3_fixed"],
         width,
         label="PKB + IKA",
     )
-    ax.set_ylabel("Iterations")
-    ax.set_title(
-        "Number of configurations ahead to find\nthe best result w.r.t. the baseline"
-    )
+    ax.set_ylabel("No. configuration")
+    ax.set_title("No. configuration in which the approaches achieve the best result")
     ax.set_xticks(x, labels)
-    ax.legend()
+    # ax.legend()
 
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    lgd = fig.legend(
+        by_label.values(),
+        by_label.keys(),
+        loc="lower center",
+        ncol=4,
+        bbox_to_anchor=(0.5, -0.05),
+    )
+    text = fig.text(-0.2, 1.05, "", transform=ax.transAxes)
     fig.tight_layout()
-    fig.savefig(os.path.join(path, f"barchart_delta_iteration.png"))
+    fig.savefig(
+        os.path.join(path, f"barchart_delta_iteration.png"),
+        bbox_extra_artists=(lgd, text),
+        bbox_inches="tight",
+    )
 
 
 path = os.path.join("/", "home", "results")
 df = pd.read_csv(os.path.join(path, "small_baseline_5000_summary.csv"))
 df = df.set_index("id")
-plot_pd(
-    df,
-    "small_baseline_5000",
-    ["hamlet_250", "hamlet_1000_kb3", "hamlet_250_kb3_fixed"],
-    path,
-)
+# plot_pd(
+#     df,
+#     "small_baseline_5000",
+#     ["hamlet_250", "hamlet_1000_kb3", "hamlet_250_kb3_fixed"],
+#     path,
+# )
 plot_matplotlib(
     df,
     "small_baseline_5000",
