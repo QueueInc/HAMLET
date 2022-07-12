ccb3d6f4eea94f5d9fb603a678d9a86eec2a03b9
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-07-12 10:22:46 +0200

    Update process_results.py

--- automl/post_processor/process_results.py
+++ automl/post_processor/process_results.py
@@ -6,12 +6,38 @@ import pandas as pd
 def extract_results(path, iteration):
     results = {}
     for root, dirs, files in os.walk(path):
+
         if f"automl_output_{iteration}.json" in files:
-            # Opening JSON file
-            with open(
-                os.path.join(root, f"automl_output_{iteration}.json")
-            ) as json_file:
-                results[root.split("/")[4]] = json.load(json_file)
+            results[root.split("/")[4]] = {
+                "graph_generation_time": 0,
+                "space_generation_time": 0,
+                "optimization_time": 0,
+                "mining_time": 0,
+            }
+            for it in range(1, iteration + 1):
+                if f"automl_output_{it}.json" in files:
+                    # Opening JSON file
+                    with open(
+                        os.path.join(root, f"automl_output_{it}.json")
+                    ) as json_file:
+                        loaded = json.load(json_file)
+                        if it == iteration:
+                            current_json = results[root.split("/")[4]]
+                            results[root.split("/")[4]] = loaded
+                        else:
+                            current_json = loaded
+                        results[root.split("/")[4]][
+                            "graph_generation_time"
+                        ] += current_json["graph_generation_time"]
+                        results[root.split("/")[4]][
+                            "space_generation_time"
+                        ] += current_json["space_generation_time"]
+                        results[root.split("/")[4]][
+                            "optimization_time"
+                        ] += current_json["optimization_time"]
+                        results[root.split("/")[4]]["mining_time"] += current_json[
+                            "mining_time"
+                        ]
 
     with open(os.path.join(path, "summary.json"), "w") as outfile:
         json.dump(results, outfile, indent=4)
@@ -52,7 +78,13 @@ def summarize(baseline, other, exhaustive, limit):
                     if limit is None or approach == exhaustive
                     else get_best_in(limit, result["evaluated_rewards"])
                 )
-                data[dataset][approach] = round(temp * 100, 2)
+                data[dataset][approach] = round(temp, 3)
+                data[dataset][f"argumentation_time_{approach}"] = (
+                    result["graph_generation_time"] + result["space_generation_time"]
+                )
+                data[dataset][f"automl_time_{approach}"] = (
+                    result["optimization_time"] + result["mining_time"]
+                )
                 data[dataset][f"iteration_{approach}"] = get_position(
                     temp, result["evaluated_rewards"]
                 )
@@ -72,28 +104,28 @@ def summarize(baseline, other, exhaustive, limit):
                         - data[dataset][f"iteration_{baseline}"]
                     )
 
-                    data[dataset][f"norm_iteration_{approach}"] = round(
-                        data[dataset][f"delta_iteration_{approach}"]
-                        / min(1000, data[dataset][f"tot_iteration_{baseline}"]),
-                        2,
-                    )
+                    # data[dataset][f"norm_iteration_{approach}"] = round(
+                    #     data[dataset][f"delta_iteration_{approach}"]
+                    #     / min(1000, data[dataset][f"tot_iteration_{baseline}"]),
+                    #     2,
+                    # )
 
                     data[dataset][f"delta_{approach}"] = round(
-                        (data[dataset][approach] - data[dataset][baseline]), 2
+                        (data[dataset][approach] - data[dataset][baseline]), 3
                     )
 
-                    if exhaustive in data[dataset]:
-                        data[dataset][f"norm_{approach}"] = round(
-                            (
-                                (
-                                    data[dataset][f"delta_{approach}"]
-                                    if abs(data[dataset][f"delta_{approach}"]) > 2
-                                    else 0.0
-                                )
-                                / (data[dataset][exhaustive] - data[dataset][baseline])
-                            ),
-                            2,
-                        )
+                    # if exhaustive in data[dataset]:
+                    #     data[dataset][f"norm_{approach}"] = round(
+                    #         (
+                    #             (
+                    #                 data[dataset][f"delta_{approach}"]
+                    #                 if abs(data[dataset][f"delta_{approach}"]) > 0.02
+                    #                 else 0.0
+                    #             )
+                    #             / (data[dataset][exhaustive] - data[dataset][baseline])
+                    #         ),
+                    #         2,
+                    #     )
                 else:
                     import datetime
 
@@ -101,11 +133,18 @@ def summarize(baseline, other, exhaustive, limit):
                         datetime.timedelta(seconds=result["optimization_time"])
                     )
 
-    df = pd.DataFrame.from_dict(data, orient="index").sort_values(
-        [f"delta_{x}" for x in other], ascending=False
-    )
+    df = pd.DataFrame.from_dict(data, orient="index")
+    # .sort_values(
+    #     [f"delta_{x}" for x in other], ascending=False
+    # )
     mode = f"small_{baseline}"
-    # TODO: rename unnamed to id
+
+    mf = pd.read_csv(
+        os.path.join("/", "home", "resources", "dataset-meta-features.csv")
+    )
+    mf["did"] = mf["did"].astype("str")
+    mf = mf.set_index("did")
+    df = pd.concat([df, mf], axis=1, join="inner")
     df = df.loc[["40983", "40499", "1485", "1478", "1590"]]  # "554"
     df.index.names = ["id"]
     df = df.reset_index()
@@ -130,7 +169,7 @@ extract_results(os.path.join(path, "hamlet_250"), 4)
 # extract_results(os.path.join(path, "baseline_1000_218"), 1)
 # extract_results(os.path.join(path, "baseline_1000_kb3"), 1)
 # extract_results(os.path.join(path, "hamlet_250_kb3"), 4)
-# extract_results(os.path.join(path, "exhaustive"), 1)
+extract_results(os.path.join(path, "exhaustive"), 1)
 extract_results(os.path.join(path, "hamlet_1000_kb3"), 1)
 extract_results(os.path.join(path, "hamlet_250_kb3_fixed"), 4)
 # extract_results(os.path.join(path, "hamlet_150"), 6)
