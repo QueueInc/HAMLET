6a7d6afae7883ab9be7f79eb47debe61176dbb69
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-07-26 16:11:36 +0200

    Update plotter.py

--- automl/post_processor/plotter.py
+++ automl/post_processor/plotter.py
@@ -34,6 +34,18 @@ def plot_pd(df, mode, other, path):
 
 def plot_matplotlib(df, mode, other, path):
 
+    SMALL_SIZE = 14
+    MEDIUM_SIZE = 14
+    BIGGER_SIZE = 16
+
+    plt.rc("font", size=MEDIUM_SIZE)  # controls default text sizes
+    plt.rc("axes", titlesize=MEDIUM_SIZE)  # fontsize of the axes title
+    plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+    plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc("legend", fontsize=MEDIUM_SIZE)  # legend fontsize
+    plt.rc("figure", titlesize=MEDIUM_SIZE)  # fontsize of the figure title
+
     df[
         [f"argumentation_time_{x}" for x in other] + [f"automl_time_{x}" for x in other]
     ].plot.bar().get_figure().savefig(os.path.join(path, f"time_{mode}.png"))
@@ -47,8 +59,8 @@ def plot_matplotlib(df, mode, other, path):
     ax.bar(x - width / 2, df["hamlet_1000_kb3"], width, label="PKB")
     ax.bar(x + width / 2, df["hamlet_250"], width, label="IKA")
     ax.bar(x + (width / 2 * 3), df["hamlet_250_kb3_fixed"], width, label="PKB + IKA")
-    ax.set_ylabel("Balanced accuracy")
-    ax.set_title("Balanced accuracy achieved by the approaches")
+    ax.set_ylabel("Balanced accuracy", labelpad=10)
+    # ax.set_title("Balanced accuracy achieved by the approaches")
     ax.set_xticks(x, labels)
     ax.set_ylim([0.75, 1])
     # ax.legend()
@@ -60,7 +72,7 @@ def plot_matplotlib(df, mode, other, path):
         by_label.keys(),
         loc="lower center",
         ncol=4,
-        bbox_to_anchor=(0.5, -0.05),
+        bbox_to_anchor=(0.5, -0.1),
     )
     text = fig.text(-0.2, 1.05, "", transform=ax.transAxes)
     fig.tight_layout()
@@ -83,8 +95,8 @@ def plot_matplotlib(df, mode, other, path):
         width,
         label="PKB + IKA",
     )
-    ax.set_ylabel("No. configuration")
-    ax.set_title("No. configuration in which the approaches achieve the best result")
+    ax.set_ylabel("#explored pipeline instances", labelpad=10)
+    # ax.set_title("No. configuration in which the approaches achieve the best result")
     ax.set_xticks(x, labels)
     # ax.legend()
 
@@ -95,7 +107,7 @@ def plot_matplotlib(df, mode, other, path):
         by_label.keys(),
         loc="lower center",
         ncol=4,
-        bbox_to_anchor=(0.5, -0.05),
+        bbox_to_anchor=(0.5, -0.1),
     )
     text = fig.text(-0.2, 1.05, "", transform=ax.transAxes)
     fig.tight_layout()
