07d0fb2d8172c25c1999e56823cc04bb109d0dd6
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-10-07 14:13:50 +0200

    Auto-sklearn

---
+++ automl/autosklearn_main.py
@@ -0,0 +1,90 @@
+import subprocess
+import openml
+import math
+import os
+import argparse
+
+import autosklearn.classification
+
+import numpy as np
+import pandas as pd
+
+from multiprocessing import Pool
+from tqdm import tqdm
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import StratifiedKFold
+from smac.optimizer.smbo import SMBO
+from smac.runhistory.runhistory import RunInfo, RunValue
+
+i = 0
+
+metric = "balanced_accuracy"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Automated Machine Learning Workflow creation and configuration"
+    )
+    parser.add_argument(
+        "-id",
+        "--id",
+        nargs="?",
+        type=str,
+        required=False,
+    )
+    args = parser.parse_args()
+    return args
+
+
+def callback(smbo: SMBO, run_info: RunInfo, result: RunValue, time_left: float) -> bool:
+    global i
+    i += 1
+    return i <= 1000
+
+
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    return directory
+
+
+args = parse_args()
+dataset = openml.datasets.get_dataset(args.id)
+path = create_directory(os.path.join("resources", "auto-sklearn", dataset.name))
+
+print(dataset.name)
+print()
+
+X, y, _, _ = dataset.get_data(
+    dataset_format="array", target=dataset.default_target_attribute
+)
+
+cls = autosklearn.classification.AutoSklearnClassifier(
+    time_left_for_this_task=7200,
+    resampling_strategy=StratifiedKFold(n_splits=10),
+    metric=autosklearn.metrics.balanced_accuracy,
+    get_trials_callback=callback,
+    seed=42,
+)
+
+cls.fit(X.copy(), y.copy(), dataset_name=dataset.name)
+
+try:
+    score = round(
+        pd.DataFrame(cls.cv_results_)
+        .sort_values("rank_test_scores")
+        .iloc[0]["mean_test_score"]
+        * 100,
+        2,
+    )
+    print(score)
+    pd.DataFrame(cls.show_models()).T.to_csv(os.path.join(path, "models_details.csv"))
+    pd.DataFrame(cls.cv_results_).to_csv(os.path.join(path, "cv_results.csv"))
+    pd.DataFrame(cls.performance_over_time_).to_csv(
+        os.path.join(path, "performance_over_time.csv")
+    )
+except Exception as e:
+    print(e)
+else:
+    print(cls.sprint_statistics())
---
+++ automl/run_autosklearn.py
@@ -0,0 +1,72 @@
+import subprocess
+import openml
+import math
+import os
+import argparse
+
+import autosklearn.classification
+
+import numpy as np
+import pandas as pd
+
+import utils.istarmap  # import to apply patch
+
+from multiprocessing import Pool
+from tqdm import tqdm
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import StratifiedKFold
+from smac.optimizer.smbo import SMBO
+from smac.runhistory.runhistory import RunInfo, RunValue
+
+
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    return directory
+
+
+def get_commands(dataset_ids):
+    path = create_directory(os.path.join("resources", "auto-sklearn"))
+    return [
+        (
+            f"python automl/autosklearn_main.py --id {id}",
+            os.path.join(
+                create_directory(
+                    os.path.join(path, openml.datasets.get_dataset(id).name)
+                ),
+                f"std_out.txt",
+            ),
+            os.path.join(
+                create_directory(
+                    os.path.join(path, openml.datasets.get_dataset(id).name)
+                ),
+                f"std_err.txt",
+            ),
+        )
+        for id in dataset_ids
+    ]
+
+
+def run_cmd(cmd, stdout_path, stderr_path):
+    """Run a command in the shell, and save std out and std err.
+    Args:
+        cmd (string): the command to run
+        stdout_path (str, bytes or os.PathLike object): where to save the std out.
+        stderr_path (str, bytes or os.PathLike object): where to save the std err.
+    """
+    open(stdout_path, "w")
+    open(stderr_path, "w")
+    with open(stdout_path, "a") as log_out:
+        with open(stderr_path, "a") as log_err:
+            subprocess.call(cmd, stdout=log_out, stderr=log_err, bufsize=0, shell=True)
+
+
+dataset_ids = ["40983", "40499", "1485", "1478", "1590"]
+commands = get_commands(dataset_ids)
+
+with tqdm(total=len(dataset_ids)) as pbar:
+    with Pool(len(dataset_ids)) as pool:
+        # Assign the commands (tasks) to the pool, and run it
+        for _ in pool.istarmap(run_cmd, commands):
+            pbar.update()
