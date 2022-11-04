0fa9c3c430164f3061bedccb3408e74a045a3eb6
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-11-04 10:08:49 +0100

    Fix space loading and mining after exteding space

--- automl/utils/commons.py
+++ automl/utils/commons.py
@@ -4,13 +4,24 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.linear_model import LogisticRegression
+
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
 
 algorithms = [
-    "GaussianNB",
     "KNeighborsClassifier",
     "DecisionTreeClassifier",
+    "GaussianNB",
     "MLPClassifier",
     "SVC",
     "RandomForestClassifier",
     "AdaBoostClassifier",
+    "ExtraTreesClassifier",
+    "LogisticRegression",
+    "XGBClassifier",
+    "LGBMClassifier",
+    "CatBoostClassifier"
 ]
