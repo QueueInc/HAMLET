f16741f4522c2fa49e2dd9a36192e2b6176b008e
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-11-07 13:00:49 +0100

    Update seq scripts

--- scripts/run_hamlet_seq_500.sh
+++ scripts/run_hamlet_seq_500.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# sudo ./scripts/run_hamlet.sh results/baseline_1000_ext balanced_accuracy max 500 3600 0.2.17 1 $(pwd)/resources/kb_extended.txt
-# sudo ./scripts/run_hamlet.sh results/pkb_1000_ext balanced_accuracy max 500 3600 0.2.17 1 $(pwd)/resources/pkb_extended.txt
-sudo ./scripts/run_hamlet.sh results/ika_1000_ext balanced_accuracy max 125 900 0.2.17 4 $(pwd)/resources/kb_extended.txt
-sudo ./scripts/run_hamlet.sh results/pkb_ika_1000_ext balanced_accuracy max 125 900 0.2.17 4 $(pwd)/resources/pkb_extended.txt
+sudo chmod 777 scripts/*
+# sudo ./scripts/run_hamlet.sh results/baseline_1000_ext balanced_accuracy max 500 3600 0.2.18 1 $(pwd)/resources/kb_extended.txt
+# sudo ./scripts/run_hamlet.sh results/pkb_1000_ext balanced_accuracy max 500 3600 0.2.18 1 $(pwd)/resources/pkb_extended.txt
+sudo ./scripts/run_hamlet.sh results/ika_1000_ext balanced_accuracy max 125 900 0.2.18 4 $(pwd)/resources/kb_extended.txt
+sudo ./scripts/run_hamlet.sh results/pkb_ika_1000_ext balanced_accuracy max 125 900 0.2.18 4 $(pwd)/resources/pkb_extended.txt
