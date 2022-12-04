059d4f632c8052dd6bad4017f60942d1171e1cf4
Joseph Giovanelli <41596745+josephgiovanelli@users.noreply.github.com>
2022-12-04 14:09:53 +0100

    Automate all post processors

---
+++ scripts/run_postprocessors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# IMPROVED
+python automl/post_processor/etl.py --input-folder improved_1000 --output-folder improved_1000 --budget 1000
+python automl/post_processor/etl.py --input-folder improved_1000 --output-folder improved_500 --budget 500
+python automl/post_processor/etl.py --input-folder new_improved_500 --output-folder new_improved_500 --budget 500
+python automl/post_processor/etl.py --input-folder new_improved_seed_1000 --output-folder new_improved_seed_1000 --budget 1000
+python automl/post_processor/etl.py --input-folder new_improved_seed_1000 --output-folder new_improved_seed_500 --budget 500
+python automl/post_processor/etl.py --input-folder new_improved_svc_1000 --output-folder new_improved_svc_1000 --budget 1000
+python automl/post_processor/etl.py --input-folder new_improved_svc_1000 --output-folder new_improved_svc_500 --budget 500
+
+# NOLIMIT
+python automl/post_processor/etl.py --input-folder nolimit_1000 --output-folder nolimit_1000 --budget 1000
+python automl/post_processor/etl.py --input-folder nolimit_1000 --output-folder nolimit_500 --budget 500
+python automl/post_processor/etl.py --input-folder new_nolimit_1000 --output-folder new_nolimit_1000 --budget 1000
+python automl/post_processor/etl.py --input-folder new_nolimit_1000 --output-folder new_nolimit_500 --budget 500
+python automl/post_processor/etl.py --input-folder new_nolimit_svc_1000 --output-folder new_nolimit_svc_1000 --budget 1000
+python automl/post_processor/etl.py --input-folder new_nolimi   t_svc_1000 --output-folder new_nolimit_svc_500 --budget 500
