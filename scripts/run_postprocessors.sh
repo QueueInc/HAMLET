c73091343974dcfa040d6d3ba2d0e795320d477f
Joseph Giovanelli <41596745+josephgiovanelli@users.noreply.github.com>
2022-12-07 11:15:51 +0100

    Update run_postprocessors.sh

--- scripts/run_postprocessors.sh
+++ scripts/run_postprocessors.sh
@@ -9,6 +9,8 @@ python automl/post_processor/etl.py --input-folder new_improved_svc_1000 --outpu
 python automl/post_processor/etl.py --input-folder new_improved_svc_1000 --output-folder new_improved_svc_500 --budget 500
 python automl/post_processor/etl.py --input-folder new_improved_svc_seed_500 --output-folder new_improved_svc_seed_500 --budget 500
 python automl/post_processor/etl.py --input-folder new_improved_svc_it_1000 --output-folder new_improved_svc_it_1000 --budget 1000
+python automl/post_processor/etl.py --input-folder new_improved_svc_data_500 --output-folder new_improved_svc_data_500 --budget 500
+python automl/post_processor/etl.py --input-folder new_improved_svc_data_1000 --output-folder new_improved_svc_data_1000 --budget 1000
 
 # NOLIMIT
 python automl/post_processor/etl.py --input-folder nolimit_1000 --output-folder nolimit_1000 --budget 1000
