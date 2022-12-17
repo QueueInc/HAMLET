59d287585b0c6fc9173e2b3398ff07cdd0ee7895
Joseph Giovanelli <41596745+josephgiovanelli@users.noreply.github.com>
2022-12-17 10:56:55 +0100

    Trying to automate experiments running

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -1,7 +1,7 @@
-sudo ./scripts/run_hamlet.sh results/hamlet balanced_accuracy max 500 3600 1.0.0 1 $(pwd)/resources/kb.txt
-sudo ./scripts/run_hamlet.sh results/hamlet balanced_accuracy max 500 3600 1.0.0 1 $(pwd)/resources/pkb.txt
-sudo ./scripts/run_hamlet.sh results/hamlet balanced_accuracy max 125 900 1.0.0 4 $(pwd)/resources/kb.txt
-sudo ./scripts/run_hamlet.sh results/hamlet balanced_accuracy max 125 900 1.0.0 4 $(pwd)/resources/pkb.txt
-sudo ./scripts/run_comparison.sh h2o_500 3600 results/h2o
-sudo ./scripts/run_comparison.sh auto_sklearn_500 3600 results/auto_sklearn
+./scripts/run_hamlet.sh results/hamlet balanced_accuracy max 500 3600 1.0.0 1 $(pwd)/resources/kb.txt
+./scripts/run_hamlet.sh results/hamlet balanced_accuracy max 500 3600 1.0.0 1 $(pwd)/resources/pkb.txt
+./scripts/run_hamlet.sh results/hamlet balanced_accuracy max 125 900 1.0.0 4 $(pwd)/resources/kb.txt
+./scripts/run_hamlet.sh results/hamlet balanced_accuracy max 125 900 1.0.0 4 $(pwd)/resources/pkb.txt
+./scripts/run_comparison.sh h2o_500 3600 results/h2o
+./scripts/run_comparison.sh auto_sklearn_500 3600 results/auto_sklearn
 python automl/post_processor/etl.py --input-folder hamlet --output-folder hamlet --budget 500
