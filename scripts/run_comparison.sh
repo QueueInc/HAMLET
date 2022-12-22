b1272163201cc76c2174780b482e41418286c6da
Joseph Giovanelli <41596745+josephgiovanelli@users.noreply.github.com>
2022-12-22 17:39:44 +0100

    Clean repo

--- scripts/run_comparison.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker stop hamlet_just_automl
-docker rm hamlet_just_automl
-docker build -t hamlet_just_automl .
-docker run --name hamlet_just_automl --volume $(pwd):/home --detach -t hamlet_just_automl
-docker exec hamlet_just_automl python automl/run_comparison.py --tool $1 --budget $2 --output_folder $3
