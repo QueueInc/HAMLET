93e3b06e4c862f00d8554aefd27c7c6f75f046c6
Giuseppe Pisano <giuseppe.pisano3@studio.unibo.it>
2024-07-24 15:27:14 +0000

    test

--- automl/tests/hamlet_test.py
+++ automl/tests/hamlet_test.py
@@ -21,9 +21,9 @@ class TestHamlet(unittest.TestCase):
         args.batch_size = 25
         args.time_budget = 60
 
-        # best, _ = run(args)
+        best, _ = run(args)
 
-        # self.assertGreater(len(best), 0)
+        self.assertGreater(len(best), 0)
 
     def test_equalized_odds(self):
 
