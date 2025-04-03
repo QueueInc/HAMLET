31f0462067103b31c24d4cc1d819cb68f0c0d545
Joseph Giovanelli <j.giovanelli@unibo.it>
2025-04-03 09:36:55 +0200

    fix: hamlet_test

--- automl/tests/hamlet_test.py
+++ automl/tests/hamlet_test.py
@@ -12,10 +12,10 @@ class TestHamlet(unittest.TestCase):
         args = MagicMock()
         args.seed = 42
         args.fair_metric = "demographic_parity_difference"
-        args.sensitive_features = "8_12"
+        # args.sensitive_features = "8_12"
         args.metric = "balanced_accuracy"
-        args.input_path = "automl/resources/automl_input_1.json"
-        args.output_path = "automl/resources/automl_output_1.json"
+        args.input_path = "automl/resources/new_automl_input_1.json"
+        args.output_path = "automl/resources/new_automl_output_1.json"
         args.dataset = "31"
         args.mode = "max"
         args.batch_size = 25
@@ -30,10 +30,10 @@ class TestHamlet(unittest.TestCase):
         args = MagicMock()
         args.seed = 42
         args.fair_metric = "equalized_odds_ratio"
-        args.sensitive_features = "8_12"
+        # args.sensitive_features = "8_12"
         args.metric = "balanced_accuracy"
-        args.input_path = "automl/resources/automl_input_1.json"
-        args.output_path = "automl/resources/automl_output_1.json"
+        args.input_path = "automl/resources/new_automl_input_1.json"
+        args.output_path = "automl/resources/new_automl_output_1.json"
         args.dataset = "31"
         args.mode = "max"
         args.batch_size = 25
