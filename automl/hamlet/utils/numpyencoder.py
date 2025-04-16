d536bbb8cd3d0a804186639d276b96ea6f0f996e
Giuseppe Pisano <g.pisano@unibo.it>
2025-04-16 17:13:36 +0200

    fix: missing numpyencoder

---
+++ automl/hamlet/utils/numpyencoder.py
@@ -0,0 +1,29 @@
+import json
+import numpy as np
+
+# https://github.com/hmallen/numpyencoder/blob/f8199a61ccde25f829444a9df4b21bcb2d1de8f2/numpyencoder/numpyencoder.py
+class NumpyEncoder(json.JSONEncoder):
+    """ Custom encoder for numpy data types """
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+                            np.int16, np.int32, np.int64, np.uint8,
+                            np.uint16, np.uint32, np.uint64)):
+
+            return int(obj)
+
+        if isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+            return float(obj)
+        
+        if isinstance(obj, (np.complex_, np.complex64, np.complex128)):
+            return {'real': obj.real, 'imag': obj.imag}
+        
+        if isinstance(obj, (np.ndarray,)):
+            return obj.tolist()
+    
+        if isinstance(obj, (np.bool_)):
+            return bool(obj)
+
+        if isinstance(obj, (np.void)): 
+            return None
+
+        return json.JSONEncoder.default(self, obj)
